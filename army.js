db617fc514f9d57ca0da0d111cbc87aabbe6abfd
fydji <33400594+fydji@users.noreply.github.com>
2018-01-26 15:25:00 +0300

    starwars

---
+++ army.js
@@ -0,0 +1,260 @@
+var armyArray = [
+	{
+	  id: 1,
+	  name: "Darth Vader",
+	  post: "Supreme Commander",
+	  image: "darthvader.png",
+	},
+	{
+	  id: 2,
+	  name: "Wilhuf Tarkin",
+	  post: "Grand Moff",
+	  image: "wilhuftarkin.png",
+	  parent: 1
+	},
+	{
+	  id: 3,
+	  name: "Olson Krennic",
+	  post: "Director",
+	  image: "olsonkrennic.png",
+	  parent: 1
+	},
+	{
+	  id: 4,
+	  name: "Esmin Kesig",
+	  post: "Admiral",
+	  image: "esminkesig.png",
+	  parent: 1
+	},
+	{
+	  id: 5,
+	  name: "Phasma",
+	  post: "Captain",
+	  image: "phasma.png",
+	  parent: 2
+	},
+	{
+	  id: 6,
+	  name: "Will Shepard",
+	  post: "Commander",
+	  image: "willshepard.png",
+	  parent: 2
+	},
+	{
+	  id: 7,
+	  name: "Allin Prohq",
+	  post: "Ensign",
+	  image: "allinprohq.png",
+	  parent: 2
+	},
+	{
+	  id: 8,
+	  name: "Jah Batut",
+	  post: "Admiral",
+	  image: "jahbatut.png",
+	  parent: 2
+	},
+	{
+	  id: 9,
+	  name: "FN-23",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 2
+	},
+	{
+	  id: 10,
+	  name: "FN-24",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 2
+	},
+	{
+	  id: 11,
+	  name: "FN-25",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 2
+	},
+	{
+	  id: 12,
+	  name: "FN-26",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 2
+	},
+	{
+	  id: 13,
+	  name: "RN-46",
+	  post: "Soldier",
+	  image: "stromtrooper2.png",
+	  parent: 2
+	},
+	{
+	  id: 37,
+	  name: "RN-35",
+	  post: "Soldier",
+	  image: "stromtrooper2.png",
+	  parent: 2
+	},
+	{
+	  id: 14,
+	  name: "RN-36",
+	  post: "Soldier",
+	  image: "stromtrooper2.png",
+	  parent: 2
+	},
+	{
+	  id: 15,
+	  name: "Darth Sidius",
+	  post: "Galactic Emperor",
+	  image: "darthsidius.png",
+	},
+	{
+	  id: 16,
+	  name: "PN-65",
+	  post: "Soldier",
+	  image: "stromtrooper2.png",
+	  parent: 3
+	},
+	{
+	  id: 17,
+	  name: "PN-66",
+	  post: "Soldier",
+	  image: "stromtrooper2.png",
+	  parent: 3
+	},
+	{
+	  id: 18,
+	  name: "PN-67",
+	  post: "Soldier",
+	  image: "stromtrooper2.png",
+	  parent: 3
+	},
+	{
+	  id: 19,
+	  name: "TN-11",
+	  post: "Soldier",
+	  image: "stromtrooper2.png",
+	  parent: 3
+	},
+	{
+	  id: 20,
+	  name: "TN-12",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 3
+	},
+	{
+	  id: 21,
+	  name: "TN-13",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 3
+	},
+	{
+	  id: 22,
+	  name: "TN-14",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 3
+	},
+	{
+	  id: 23,
+	  name: "WN-23",
+	  post: "Soldier",
+	  image: "stromtrooper2.png",
+	  parent: 3
+	},
+	{
+	  id: 24,
+	  name: "WN-24",
+	  post: "Soldier",
+	  image: "stromtrooper2.png",
+	  parent: 3
+	},
+	{
+	  id: 25,
+	  name: "WN-25",
+	  post: "Soldier",
+	  image: "stromtrooper2.png",
+	  parent: 3
+	},
+	{
+	  id: 26,
+	  name: "WN-26",
+	  post: "Soldier",
+	  image: "stromtrooper2.png",
+	  parent: 3
+	},
+	{
+	  id: 27,
+	  name: "KN-19",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 4
+	},
+	{
+	  id: 28,
+	  name: "KN-20",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 4
+	},
+	{
+	  id: 29,
+	  name: "KN-21",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 4
+	},
+	{
+	  id: 30,
+	  name: "KN-22",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 4
+	},
+	/*dobavleno*/
+	{
+	  id: 31,
+	  name: "KN-20",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 5
+	},
+	{
+	  id: 32,
+	  name: "KN-21",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 7
+	},
+	{
+	  id: 33,
+	  name: "KN-22",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 7
+	},
+	{
+	  id: 34,
+	  name: "KN-20",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 25
+	},
+	{
+	  id: 35,
+	  name: "KN-21",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 25
+	},
+	{
+	  id: 36,
+	  name: "KN-22",
+	  post: "Soldier",
+	  image: "stromtrooper.png",
+	  parent: 30
+	}
+];
---
+++ script.js
@@ -0,0 +1,172 @@
+var listWarriors = document.getElementById('listWarriors');
+var levelUp = document.getElementById('levelUp');
+var originPage = document.getElementById('originPage');
+var nextWarrior = document.getElementById('nextWarrior');
+var previosWarrior = document.getElementById('previosWarrior');
+var idWarrior = '';
+var targetWarrior = '';
+var armyList= '';
+var countArmyList= '';
+var warriorListByParent ='';
+var targetWarriorIndex = '';
+var sum = '';
+
+if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+  console.log('this is a touch device');
+} 
+else {
+  console.log('this is not a touch device');
+  document.body.classList.add('no-touch');
+}
+
+function findDataWarrior() {
+	return armyArray.filter(function(onlyOne) {
+		return onlyOne.id === +idWarrior;
+	});
+};
+
+function outDataWarrior() {
+	$('#warriorSelected').fadeOut('300', function() {
+		document.getElementById("warriorSelected").getElementsByTagName("img")[0].src ='avatars/' + targetWarrior[0].image;	
+		document.getElementById("warriorSelected").getElementsByTagName("h1")[0].innerHTML =targetWarrior[0].name;		
+		document.getElementById("warriorSelected").getElementsByTagName("p")[0].innerHTML = targetWarrior[0].post;
+	});
+	$('#warriorSelected').fadeIn(5);
+};
+
+function getChildList(currentId) {
+	return armyArray.filter(function(list) {
+		return list.parent === +currentId.id;
+	});
+};
+
+function getChildCount(object) {
+	var listChild = getChildList(object);
+	if (listChild.length > 0) {
+		sum += listChild.length;
+		listChild.forEach(function(item) {
+			object = item;
+			getChildCount(item);
+		});
+	};
+};
+
+function outArmyList() {
+	var out='';
+	for (var j = 0;j < armyList.length;j++){
+		sum = 0;
+		getChildCount(armyList[j]);
+		if (sum > 0) {
+			out += '<section class="list_item" data-id="' + armyList[j].id + '">' + '<img class="item_img"' + '" src="avatars/' + armyList[j].image + '" />' + '<span class="counter" style="visibility: visible">' + sum +  '</span>' + '<span class="item"><h2 class="item_title">' + armyList[j].name + '</h2>' + '<p class="item_description">' + armyList[j].post + '</p>' + '</section>';
+			}
+		else {
+			out += '<section class="list_item" data-id="' + armyList[j].id + '">' + '<img class="item_img"' + '" src="avatars/' + armyList[j].image + '" />' + '<span class="counter" style="visibility: hidden">' + sum +  '</span>' + '<span class="item"><h2 class="item_title">' + armyList[j].name + '</h2>' + '<p class="item_description">' + armyList[j].post + '</p>' + '</section>';
+			}
+	}
+	$('#listWarriors').fadeOut('10', function() {
+		document.getElementById('listWarriors').innerHTML = out; 
+		});
+	$('#listWarriors').fadeIn(5);
+};
+
+listWarriors.onclick = function(event) {
+	var target = event.target;
+	var targetSection = target.closest('section');
+	if (!targetSection) return;
+	if (!listWarriors.contains(targetSection)) return;
+	idWarrior = targetSection.getAttribute('data-id');
+	targetWarrior = findDataWarrior();
+	outDataWarrior();
+	armyList = getChildList(targetWarrior[0]);
+	outArmyList();
+	$('#previosWarrior, #nextWarrior').fadeOut('10', function() {
+		document.getElementById('previosWarrior').style= "visibility: visible";
+		document.getElementById('nextWarrior').style= "visibility: visible";
+		document.getElementById('levelUp').style= "visibility: visible";
+	});
+};
+
+function getOriginPage() {
+	$("article").fadeOut('10', function() {
+		document.getElementById("warriorSelected").getElementsByTagName("h1")[0].innerHTML = "Galactic Empire";		
+		document.getElementById("warriorSelected").getElementsByTagName("p")[0].innerHTML = "Imperial military";
+		document.getElementById("warriorSelected").getElementsByTagName("img")[0].src ='avatars/empire.png';
+		 });
+	$("article").fadeIn(5);
+	armyList = getOriginList();
+	outArmyList();
+	$('#previosWarrior, #nextWarrior').fadeOut('10', function() {
+		document.getElementById('previosWarrior').style= "visibility: hidden";
+		document.getElementById('nextWarrior').style= "visibility: hidden";
+		document.getElementById('levelUp').style= "visibility: hidden";
+	});	
+};
+
+originPage.onclick = function(event) {
+	getOriginPage();
+};
+
+levelUp.onclick = function(event) {
+	if (targetWarrior[0].parent === undefined) {
+		getOriginPage();
+		}
+	else {
+		idWarrior = targetWarrior[0].parent;
+		targetWarrior = findDataWarrior();
+		outDataWarrior();
+		armyList = getChildList(targetWarrior[0]);
+		outArmyList()};
+};
+
+function getOriginList() {
+	return armyArray.filter(function(noParent) {
+		return noParent.parent === undefined;
+	});
+};
+
+function getWarriorListByParent () {
+	if (targetWarrior[0].parent === undefined) {
+		warriorListByParent = getOriginList();
+		}
+	else {
+		return warriorListByParent = armyArray.filter(function(listW) {
+			return listW.parent === +targetWarrior[0].parent;
+		});
+	}
+};
+
+function getWarriorId () {	
+	return targetWarriorIndex = warriorListByParent.findIndex(function(warId) {
+		return warId.id === +targetWarrior[0].id;
+	});
+};
+
+nextWarrior.onclick = function(event) {
+	getWarriorListByParent ();
+	getWarriorId ();
+	if ( targetWarriorIndex < warriorListByParent.length - 1) {
+		targetWarriorIndex++;}
+	else {
+		targetWarriorIndex = 0;
+	}
+	idWarrior = warriorListByParent[targetWarriorIndex].id;
+	targetWarrior = findDataWarrior();
+	outDataWarrior();
+	armyList = getChildList(targetWarrior[0]);
+	outArmyList();
+};	
+
+previosWarrior.onclick = function(event) {
+	getWarriorListByParent ();
+	getWarriorId ();
+	if ( targetWarriorIndex > 0) {
+		targetWarriorIndex--;}
+	else {
+		targetWarriorIndex = warriorListByParent.length - 1;
+	}
+	idWarrior = warriorListByParent[targetWarriorIndex].id;
+	targetWarrior = findDataWarrior();
+	outDataWarrior();
+	armyList = getChildList(targetWarrior[0]);
+	outArmyList();
+};
